8dc043b8fdfbe54314cfaf61d125197b6753acf5
Kolia <admin@admin.ua>
2024-10-21 20:38:09 +0300

    #183:lab 2.2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog', 
 ]
 
 MIDDLEWARE = [
@@ -76,9 +77,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # або os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+
+
 
 
 # Password validation
